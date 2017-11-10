74752888ad9789a20211fdbb7f8f60a9183057e9
S7tH <aurelien.theriot@gmail.com>
2017-11-10 16:43:24 +0100

    added a functional test

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -23,6 +23,25 @@ class TaskControllerTest extends AbstractControllerTest
         //echo $this->client->getResponse()->getContent();
     }
 
+    public function testTaskListWithRequestIsUp()
+    {
+        //user is authenticated
+        $this->logUser();
+
+        /*call the request method with 2 parameters
+        1/ the http method 2/ the uri what we want to recover*/
+        $this->client->request('GET', '/tasks?overtasks=1');
+
+        //recover the response with status code
+        $response = $this->client->getResponse()->getStatusCode();
+
+        //The test with the code status expected
+        $this->assertSame(200,$response);
+
+        //To display the html code remove the double slash ahead the following line
+        //echo $this->client->getResponse()->getContent();
+    }
+
     public function testTaskCreatepageIsUp()
     {
         //user is authenticated
